551a3e6aa70a83734fc7038ac4027fc950a09aa5
novakiss <tanbmhn@gmail.com>
2018-05-28 13:40:13 +0200

    First Commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {
+    View,
+    Text
+} from 'react-native';
+
+
+
+export default class App extends Component{
+    render() {
+        return (
+            <View>
+                <Text style={{marginTop:100}}>
+                    Hello React Native
+                </Text>
+            </View>
+        );
+    }
+}
