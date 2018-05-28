1abf3d05f24faecc2a2628249e1ed26a1deb8390
novakiss <tanbmhn@gmail.com>
2018-05-28 14:08:05 +0200

    StyleSheet in React-Native

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
 import React, {Component} from 'react';
 import {
     View,
-    Text
+    Text, StyleSheet
 } from 'react-native';
 
 
-
-export default class App extends Component{
+export default class App extends Component {
     render() {
         return (
             <View>
-                <Text style={{marginTop:100}}>
+                <Text style={ao.tieude}>
                     Hello React Native
+                </Text>
+                <Text style={ao.ten}>
+                    My name is Novakiss
                 </Text>
             </View>
         );
     }
 }
+
+const ao = StyleSheet.create({
+    tieude: {
+        backgroundColor: 'yellow',
+        color: 'red'
+    },
+    ten: {
+        backgroundColor: 'green',
+        color: 'yellow'
+    }
+});
