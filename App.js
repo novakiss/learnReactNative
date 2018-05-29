80990b5647386014bfd149d36834cbafe4b7c968
novakiss <tanbmhn@gmail.com>
2018-05-29 15:25:02 +0200

    State done

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import React, {Component} from 'react';
 import {
-    View,TouchableOpacity,Text
+    View, Text, TouchableOpacity
 } from 'react-native';
 
 
-export default class App extends Component{
-    clickMe=()=>{
-      console.log('Clicked')
+export default class App extends Component {
+    state = {
+        number: 100
     };
+
+    clickMe = () => {
+        this.setState({number: this.state.number + 1})
+    };
+
     render() {
         return (
             <View>
-                <TouchableOpacity onPress={this.clickMe}>
-                <View style={{backgroundColor:'green',width:100,height:20,marginTop:100,marginLeft:20}}>
-                    <Text>Click Me</Text>
-                </View>
+                <Text style={{fontSize: 100}}>{this.state.number}</Text>
+                <TouchableOpacity style={{backgroundColor: 'red', width: 100, height: 20}} onPress={this.clickMe}>
+                    <View>
+                        <Text>KlickMe</Text>
+                    </View>
                 </TouchableOpacity>
             </View>
         );
